4437fa2c22022a583fbf0c873d1c5b1262c30497
Alex Carruthers <alex@alexcarruthers.com>
2013-12-19 10:18:04 -0500

    Fixed Unicode support, also formatted for pep8

--- gtfs/loader.py
+++ gtfs/loader.py
@@ -3,42 +3,43 @@ import feed
 from entity import *
 import sys
 
+
 def load(feed_filename, db_filename=":memory:"):
-  schedule = Schedule( db_filename ) 
-  schedule.create_tables()
-  
-  fd = feed.Feed( feed_filename )
+    schedule = Schedule(db_filename)
+    schedule.create_tables()
 
-  for gtfs_class in (Agency, 
-                     Route, 
-		     Stop,
-		     Trip, 
-		     StopTime,
-		     ServicePeriod, 
-		     ServiceException, 
-		     Fare,
-		     FareRule,
-		     ShapePoint,
-		     Frequency,
-		     Transfer,
-		     ):
+    fd = feed.Feed(feed_filename)
 
-    print "loading %s"%gtfs_class
-   
-    try:
-      for i, record in enumerate( fd.get_table( gtfs_class.TABLENAME+".txt" ) ):
-        if i%500==0:
-	  sys.stdout.write(".")
-	  sys.stdout.flush()
-	  schedule.session.commit()
+    for gtfs_class in (Agency,
+                       Route,
+                       Stop,
+                       Trip,
+                       StopTime,
+                       ServicePeriod,
+                       ServiceException,
+                       Fare,
+                       FareRule,
+                       ShapePoint,
+                       Frequency,
+                       Transfer,
+    ):
 
-        instance = gtfs_class( **record.to_dict() )
-        schedule.session.add( instance )
-      print
-    except KeyError:
-      # TODO: check if the table is required
-      continue
+        print "loading %s" % gtfs_class
 
-  schedule.session.commit()
+        try:
+            for i, record in enumerate(fd.get_table(gtfs_class.TABLENAME + ".txt")):
+                if i % 500 == 0:
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+                    schedule.session.commit()
 
-  return schedule
+                instance = gtfs_class(**record.to_dict())
+                schedule.session.add(instance)
+            print
+        except KeyError:
+            # TODO: check if the table is required
+            continue
+
+    schedule.session.commit()
+
+    return schedule
